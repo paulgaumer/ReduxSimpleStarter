82378e22f3a5694756f6750cae83480d88670e56
paulgaumer <mitsudomoe8@gmail.com>
2017-12-08 18:42:51 +0900

    End of 1-15

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-const App = function() {
-  return <div>Hi there!</div>;
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCbwXsrDW3-aaxM43RgzFkdMmGt1t2MGqA';
+
+
+
+const App = () => {
+return (
+  <div>
+    <SearchBar />
+  </div>
+);
 }
+
+ReactDOM.render(<App />, document.querySelector('.container'));
