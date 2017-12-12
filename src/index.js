d0f76cdfe2f7330051ca430d71f001fc6564d0c7
paulgaumer <mitsudomoe8@gmail.com>
2017-12-12 19:00:16 +0900

    Finished 2-33

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
@@ -15,19 +16,26 @@ class App extends Component {
       videos: [],
       selectedVideo: null 
     };
-
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    
+    this.videoSearch('surfboards')
+  }
+  
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({ 
         videos: videos,
         selectedVideo: videos[0] 
       });
     });
-  }
+  };
 
   render() {
+    // creates a delay in the search method
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
     return (
     <div>
-      <SearchBar />
+      <SearchBar onSearchTermChange = { videoSearch } />
       <VideoDetail video = { this.state.selectedVideo } />
       <VideoList 
         onVideoSelect = { (selectedVideo) => this.setState({selectedVideo}) }
